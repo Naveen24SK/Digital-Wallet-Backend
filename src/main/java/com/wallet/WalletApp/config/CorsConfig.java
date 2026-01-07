1fce149bbbf9680ae824bb97f010affaa0e7b349
Naveen24SK <sknaveenvj@gmail.com>
2026-01-07 23:52:12 +0530

    config fix

--- src/main/java/com/wallet/WalletApp/config/CorsConfig.java
+++ src/main/java/com/wallet/WalletApp/config/CorsConfig.java
@@ -15,7 +15,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-                                "http://localhost:5173",
+//                                "http://localhost:5173",
                                 "https://digitalwallet-beige.vercel.app"
                         )
                         .allowedMethods("*")
